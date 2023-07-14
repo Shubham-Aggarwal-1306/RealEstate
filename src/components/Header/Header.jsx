a4b9850ed279730e0a2d89681b0963a342c03112
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-07-14 18:56:29 +0530

    Responsive Design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import { BiMenuAltRight } from 'react-icons/bi';
+import  OutsideClickHandler from 'react-outside-click-handler';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const getMenuStyles = (menuOpen) => {
+        if (document.documentElement.clientWidth <= 768) {
+            return {
+                right: !menuOpen && '-100%',
+            }
+        }
+    }
+
     return (
         <section className="h-wrapper">
             <div className="h-container flexCenter paddings innerWidth">
                 <img src="./logo.png" alt='logo' width={100} />
-                <div className="flexCenter  h-menu">
-                    <a href="#">
-                        Residences
-                    </a>
-                    <a href="#">
-                        Our Value
-                    </a>
-                    <a href="#">
-                        Contact Us
-                    </a>
-                    <a href="#">
-                        Get Started
-                    </a>
-                    <button className="button">
-                        Contact
-                    </button>
+                <OutsideClickHandler
+                    onOutsideClick={() => 
+                    {
+                        setMenuOpen(false);
+                    }}
+                >
+                    <div className="flexCenter  h-menu" style={getMenuStyles(menuOpen)}>
+                        <a href="#">
+                            Residences
+                        </a>
+                        <a href="#">
+                            Our Value
+                        </a>
+                        <a href="#">
+                            Contact Us
+                        </a>
+                        <a href="#">
+                            Get Started
+                        </a>
+                        <button className="button">
+                            Contact
+                        </button>
+                    </div>
+                </OutsideClickHandler>
+                <div className='menu-icon' onClick={() => setMenuOpen((prev) => !prev)}>
+                    <BiMenuAltRight size={30} />
                 </div>
             </div>
         </section>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Hero.css';
 import { HiLocationMarker } from 'react-icons/hi';
 import CountUp from 'react-countup';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
@@ -11,13 +12,17 @@ const Hero = () => {
           <div className="flexColStart hero-left">
             <div className="hero-title">
               <div className="orange-circle" />
-              <h1>
+              <motion.h1
+                initial={{ opacity: 0, y: "2rem" }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 2, type: "spring" }}
+              >
                 Discover
                 <br />
                 Most Suitable
                 <br />
                 Property
-              </h1>
+              </motion.h1>
             </div>
             <div className="flexColStart hero-des">
               <span className='secondaryText'> Find a variety of properties that suit you very easilty </span>
@@ -54,9 +59,13 @@ const Hero = () => {
           </div>
 
           <div className="flexCenter hero-right">
-            <div className="image-container">
+            <motion.div
+              initial={{ opacity: 0, x: "7rem" }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 2, type: "spring" }}
+              className="image-container">
               <img src="./hero-image.png" alt="hero" />
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
